3d8545676298c416450e59c34e5b69adfb08595c
Daniel Loewenstein <loewenstein.daniel@gmail.com>
2019-06-26 21:01:38 +1000

    Remove DeepKnockoffs version check
    DeepKnockoffs library checks which version is installed, however,
    if one choose to not perform installation but instead adds to path
    these lines prevented running the software.

--- DeepKnockoffs/DeepKnockoffs/__init__.py
+++ DeepKnockoffs/DeepKnockoffs/__init__.py
@@ -1,5 +1,2 @@
-from pkg_resources import get_distribution
-__version__ = get_distribution('DeepKnockoffs').version
-
 from DeepKnockoffs.machine import KnockoffMachine
 from DeepKnockoffs.gaussian import GaussianKnockoffs
